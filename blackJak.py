10d9cbe00490f8330efb78b5076498efc985d3fc
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-10 22:31:16 -0400

    saved my changes and added some line breaks

--- blackJak.py
+++ blackJak.py
@@ -2,8 +2,12 @@ import random
 def main():
 	computerScore = 0
 	userScore = 0
+
 	while True:
 		print("Welcome to BlackJak!\n")
 		bet = int(input("How much is your wager?\n"))
 		break
+
+
+
 main()
