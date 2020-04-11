3228cb77174a2ab247cce84ae99bc55e60241eb7
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-10 22:27:23 -0400

    added random module and made better readability

--- blackJak.py
+++ blackJak.py
@@ -1,8 +1,9 @@
+import random
 def main():
 	computerScore = 0
 	userScore = 0
 	while True:
-		print("Welcome to BlackJak!")
-		bet = int(input("How much is your wager?"))
+		print("Welcome to BlackJak!\n")
+		bet = int(input("How much is your wager?\n"))
 		break
 main()
