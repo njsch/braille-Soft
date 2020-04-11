dac0c2e795382cdd2802868abde4c6f4dce6a08e
datajake1999 <datajake@gmail.com>
2020-04-10 19:45:00 -0700

    if the user enters 0 for the bet, print another error

--- blackJak.py
+++ blackJak.py
@@ -8,6 +8,8 @@ def main():
 		bet = int(input("How much is your wager?\n"))
 		if bet < 0:
 			print("Your bet needs to be more than 0.\n")
+		elif bet == 0:
+			print("DON'T BE A WISE ASS!\n")
 		break
 
 
