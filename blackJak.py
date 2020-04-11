1bbebe8ca866fd92d127f1a6c3afe9b1090be206
datajake1999 <datajake@gmail.com>
2020-04-10 19:31:13 -0700

    added error message if bet is less than 0

--- blackJak.py
+++ blackJak.py
@@ -4,5 +4,7 @@ def main():
 	while True:
 		print("Welcome to BlackJak!")
 		bet = int(input("How much is your wager?"))
+		if bet < 0:
+			print("Your bet needs to be more than 0.\n")
 		break
 main()
