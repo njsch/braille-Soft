07df3589a79533c3dfeaeb828ab63937c00a7d6c
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-10 23:07:55 -0400

    added $ for user total

--- blackJak.py
+++ blackJak.py
@@ -1,13 +1,18 @@
 import random
+
 def main():
 	computerScore = 0
 	userScore = 0
+	totalUserMoney = 30000
 
 	while True:
 		print("Welcome to BlackJak!\n")
+		print("you have","$",totalUserMoney,"\n")
 		bet = int(input("How much is your wager?\n"))
+
 		if bet < 0:
 			print("Your bet needs to be more than 0.\n")
+
 		elif bet == 0:
 			print("DON'T BE A WISE ASS!\n")
 		break
