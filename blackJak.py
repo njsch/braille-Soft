9b8fd8069bc625b8737060226ba8422e007c9d71
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-11 14:00:07 -0400

    added functions to code and started work on scoring

--- blackJak.py
+++ blackJak.py
@@ -1,15 +1,14 @@
 import random
 
 def main():
-	computerScore = 0
-	userScore = 0
-	totalUserMoney = 30000
+	bet()
 
+def bet():
+	totalUserMoney = 30000
 	while True:
 		print("Welcome to BlackJak!\n")
 		print("you have","$",totalUserMoney,"\n")
 		bet = int(input("How much is your wager?\n"))
-
 
 		if bet < 0:
 			print("Your bet needs to be more than 0.\n")
@@ -19,12 +18,17 @@ def main():
 
 		elif bet == 0:
 			print("DON'T BE A WISE ASS!\n")
-
 		else:
-			userCard = random.randint(1,7)
-			print(userCard)
-			break
+			game(bet)
 
 
+def game(bet):
+	totalUserMoney = 30000
+	userScore = 0
+	while True: 
+		userCard =  random.randint(1,10)
+		totalUserScore = userScore+1
+		print(totalUserScore)
+		break
 
 main()
