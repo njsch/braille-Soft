b3ae9924c3d0d56e51cc9501acf078968b2b45ba
datajake1999 <datajake@gmail.com>
2020-04-12 17:24:08 -0700

    started rewriting blackjack

---
+++ blackJak.py
@@ -0,0 +1,23 @@
+def main():
+	print("welcome To BlackJak!\n")
+	money = 30000
+	line = 1
+	while True:
+		card = 0
+		ds = 0
+		us = 0
+		dn = 0
+		un = 0
+		bet = 0
+		while line == 1:
+			print("\nYou have $" + str(money) + ".00.\n")
+			bet = input("How much is your wager?\n")
+			line = 2
+			break
+		while line == 2:
+			if bet == 0:
+				print("DON'T BE A WISE ASS!\n")
+				line = 1
+				break
+
+main()
