0640f88de92d238caf3db55285832e1d1f97fd7e
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-12 21:18:04 -0400

    added some features for basic gameplay for the user

--- 23bricks.py
+++ 23bricks.py
@@ -1,11 +1,25 @@
-def welcome():
+while True:
 	print("Hi! Let's play a game of twenty-three bricks.")
+	break
 
-	message = input("Do you need instructions? Type Y for Yes or N for No.\n")
+message = input("Do you need instructions? Type Y for Yes or N for No.\n")
+while message.upper() == "Y" or message.upper() == "N":
 	if message.upper() == "Y":
 		print("You have a pile of twenty-three bricks.\n")
 		print("You and another person, or the computer can take one, two, or three bricks.\n")
 		print("Whoever takes the last brick loses!\n")
+		break
 
+	elif message.upper() =="N":
+		bricks = 23
+		while bricks >0:
+			print("There are",bricks,"remaining\n")
+			bricksInput = int(input("How many bricks do you want? Choose 1, 2, or 3.\n"))
+			bricks -=bricksInput
+			print("You take",bricksInput,"leaving",bricks)
+			if bricks == 0:
+				print("Sorry, you took the last brick and lost!\n")
+				playAgain = input("Do you want to play again? Type Y for Yes or N for No.\n")
+				if playAgain == "Y":
+					continue
 
-welcome()
--- blackJak.py
+++ blackJak.py
@@ -9,11 +9,10 @@ def main():
 		dn = 0
 		un = 0
 		bet = 0
+		break
 		while line == 1:
 			print("\nYou have $" + str(money) + ".00.\n")
 			bet = input("How much is your wager?\n")
-			line = 2
-			break
 		while line == 2:
 			if bet == 0:
 				print("DON'T BE A WISE ASS!\n")
